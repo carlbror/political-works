b7ef7e73d5fc95585e87801fb88698775218253c
Carl Jakobsson <carl.bror@gmail.com>
2015-08-05 15:02:23 +0200

    Towards a functioning itt

--- client/ideological_turing_tests/itt_guess_page.js
+++ client/ideological_turing_tests/itt_guess_page.js
@@ -1,6 +1,14 @@
 Template.ittGuessPage.helpers({
-    answerToQuestion: function(){
-        console.log(this);
-        console.log(Template.parentData())
+    firstIdeologyName: function(){
+        return get_.ideologyNameFromId(this.firstIdeologyId);
+    },
+    secondIdeologyName: function(){
+        return get_.ideologyNameFromId(this.secondIdeologyId);
+    },
+    firstIdeologues: function(){
+        return return_.ideologuesFromIdeologyName(get_.ideologyNameFromId(Template.parentData().itt.firstIdeologyId)).toLowerCase();
+    },
+    secondIdeologues: function(){
+        return return_.ideologuesFromIdeologyName(get_.ideologyNameFromId(Template.parentData().itt.secondIdeologyId)).toLowerCase();
     }
 });
